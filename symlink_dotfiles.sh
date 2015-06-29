3ec6f8e7b1ecb6f40fd6f68981d428f6770374b1
jhauh <j.haughey91@gmail.com>
2015-06-29 21:52:42 +0100

    adding symlink_dotfiles.sh

---
+++ symlink_dotfiles.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+# create symbolic links between the hand-crafted config files in
+# dotfiles/configs and where they're expected to be 
+
+# move the current set of dotfiles somewhere so they dont get
+# written over, and in case you don't like the new configs
+mkdir -p ~/dotfiles_old
+
+mv ~/.config/compton.conf ~/dotfiles_old
+mv ~/.vimrc ~/dotfiles_old
+mv ~/.i3/i3status.conf ~/dotfiles_old
+mv ~/.i3/i3exit.sh ~/dotfiles_old
+mv ~/.i3/config ~/dotfiles_old
+
+echo 'dont worry if some mv commands failed above'
+
+# now symlink the shiny new config files
+ln -s ~/dotfiles/configs/vimrc ~/.vimrc
+ln -s ~/dotfiles/configs/i3status.conf ~/.i3/i3status.conf
+ln -s ~/dotfiles/configs/i3exit.sh ~/.i3/i3exit.sh
+ln -s ~/dotfiles/configs/config ~/.i3/config
+ln -s ~/dotfiles/configs/compton.conf ~/.config/compton.conf
+
+
+# this is pretty manual, could loop but idk this is easier
+
+
