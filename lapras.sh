be4e1dbcc624cde5761f8dae9accd5b6206e4b6c
fluffactually <j.haughey91@gmail.com>
2017-05-11 23:11:44 +0100

    fix lapras

--- lapras.sh
+++ lapras.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 #	/etc/profile.d/motd.sh
 #	Print the "Message of the day" 
+#$(sensors fam15h_power-pci-00c4 | grep power1 | sed 's/power1: *//g') 
 
 cat << EOF
                                        ,|
                                        ||
                                ,-"'""\`' \`._
-                              '----.     __\`....._       ██╗      █████╗ ██████╗ ██████╗  █████╗ ███████╗
-                               \`    \`.  \`. ;      \`.  ██║     ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
-                                \`.    \`.  \`   ,"\`. |  ██║     ███████║██████╔╝██████╔╝███████║███████╗
+                              '----.     __\`....._      ██╗      █████╗ ██████╗ ██████╗  █████╗ ███████╗
+                               \`    \`.  \`. ;      \`.    ██║     ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
+                                \`.    \`.  \`   ,"\`. |    ██║     ███████║██████╔╝██████╔╝███████║███████╗
                                   \`.  _.\`._   |  ' |    ██║     ██╔══██║██╔═══╝ ██╔══██╗██╔══██║╚════██║
                                   .','  ,' \`.  \`--'     ███████╗██║  ██║██║     ██║  ██║██║  ██║███████║
-                                 /.' _,'    | /           ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+                                 /.' _,'    | /         ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
                                 '/_.'       |.             
                                  \`---\`".    ||          Kernel:            $(uname -r)  
                                        |    ||          Uptime:            $(uptime -p | sed 's/up //g') 
-                                      ,'    \`|          Address:           $(ip addr show wlp8s0 | grep "inet " | awk '{print $2}')
+                                      ,'    \`|          Address:           $(ip addr show wlp2s0 | grep "inet " | awk '{print $2}')
                          _           /       |             
                         ' \`.        .'       |          Date:              $(date)
-                         .  \`._  _,'/|       |          CPU Tempurature:   $(sensors k10temp-pci-00c3 | grep temp1: | sed 's/temp1: *//g') 
-                        _|     "'  / |       '          Power Consumption: $(sensors fam15h_power-pci-00c4 | grep power1 | sed 's/power1: *//g')
+                         .  \`._  _,'/|       |          CPU Tempurature:   $(sensors | grep temp1: | sed 's/temp1: *//g') 
+                        _|     "'  / |       '          Power Consumption: Maybe one day...
                     _,-' |        /  '        .         Load Averages:     $(cat /proc/loadavg | awk '{print $1", "$2", "$3" (1, 5, 15 minutes)"}')
-                 |"'            ,'  '          \\        Memory:            $(cat /proc/meminfo | grep MemFree | awk '{print $2}') / $(cat /proc/meminfo | grep MemTotal | awk '{print $2}') KB
+                 |"'            ,'  '          \\        Memory:            $(free -h | awk /Mem/'{ print $3 " / " $2 }')
                  |   _        ,'   /            \\       Processes:         $(ps -ef | wc -l)
                  ;  '        /    j              .
             ,"--'    \`.    .      |              |         ________
@@ -38,3 +39,4 @@ cat << EOF
            .   .'_.-'
            | ,'.'
            | .\`
+EOF
