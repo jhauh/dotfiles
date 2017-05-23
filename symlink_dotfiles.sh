57e82514a45d7cf22b35544fec5bd4331724b2f6
fluffactually <j.haughey91@gmail.com>
2017-05-23 21:09:29 +0100

    add tmux.conf and adjust vimrc accordingly

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -12,6 +12,7 @@ mv ~/.vimrc ~/dotfiles_old
 mv ~/.i3/i3status.conf ~/dotfiles_old
 mv ~/.i3/i3exit.sh ~/dotfiles_old
 mv ~/.i3/config ~/dotfiles_old
+mv ~/.tmux.conf ~/dotfiles_old
 
 echo 'dont worry if some mv commands failed above'
 
@@ -21,6 +22,7 @@ ln -s ~/dotfiles/configs/i3status.conf ~/.i3/i3status.conf
 ln -s ~/dotfiles/configs/i3exit.sh ~/.i3/i3exit.sh
 ln -s ~/dotfiles/configs/config ~/.i3/config
 ln -s ~/dotfiles/configs/compton.conf ~/.config/compton.conf
+ln -s ~/dotfiles/configs/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/scripts/get_ws.sh ~/.i3/get_ws.sh
 ln -s ~/dotfiles/scripts/rename_ws.sh ~/.i3/rename_ws.sh
 
