27b05ed2ee562d1d3cf96ff8121cece8ada83ead
fluffactually <j.haughey91@gmail.com>
2018-04-04 23:57:21 +0100

    remove all the old repo files & structure

--- scripts/get_ws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read -r ws; do
-    read -r active
-    if [ "$active" = true ]; then 
-        echo "$ws"
-    fi
-done < <(i3-msg -t get_workspaces | jshon -a -e num -u -p -e focused -u)
--- scripts/rename_ws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ws=$(exec ~/.i3/get_ws.sh)
-command="i3-input -F 'rename workspace to \"$(echo $ws): %s\"' -P 'New name for this workspace: $(echo $ws): ' -f 'pango:Noto Sans 14px'"
-#command=`i3-input -F 'rename workspace to "%s"' -P 'New name for this workspace: ' -f 'pango: Noto Sans 14px'`
-
-eval $(echo $command)
-#echo $ws
