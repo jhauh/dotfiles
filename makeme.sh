e3785fad0da42b1b4fa4880ab55388fd0246884e
fluffactually <j.haughey91@gmail.com>
2018-04-05 00:00:23 +0100

    add to packagelist and simplify install

--- makeme.sh
+++ makeme.sh
@@ -16,27 +16,3 @@
 echo "installing list of packages contained in pkglist.txt"
 sudo apt-get install -y $(cat pkglist.txt) 
 
-# we need to install i3 for the dependencies it brings along
-# but really we're going to use a fork from github with extras
-echo "done! now grabbing a cool fork of the window manager we'll use"
-cd ~
-# clone the repository
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-
-# do this if you want the stable branch, skip if you want dev branch (gaps-next)
-#git checkout gaps && git pull
-
-# compile & install
-make
-sudo make install
-
-# hopefully that worked.
-echo "excelente! now grabbing a cool vim theme!"
-mkdir -p ~/.vim/colors && cd ~/.vim/colors
-wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
-
-cd ~/dotfiles
-
-echo "that went swimmingly, you should never have to run me again"
-echo "run symlink_dotfiles.sh now to put everything in the right place"
